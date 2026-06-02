04dc1adf2a1595ff1c2e4336d2936fb4278f6fee
Ggpatrik <166550544+Ggpatrik@users.noreply.github.com>
2026-06-02 16:27:49 +0200

    Počiatočné súbory pre projekt MakeCode

---
+++ main.ts
@@ -0,0 +1,28 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1024) {
+        servos.P1.run(100)
+        servos.P0.run(-100)
+    } else if (receivedNumber == 0) {
+        servos.P0.run(100)
+        servos.P1.run(-100)
+    } else if (receivedNumber == 530) {
+        servos.P1.run(100)
+        servos.P0.run(100)
+        basic.pause(1000)
+        servos.P1.stop()
+        servos.P0.stop()
+    } else if (receivedNumber == 489) {
+        servos.P0.run(-100)
+        servos.P1.run(-100)
+        basic.pause(1000)
+        servos.P1.stop()
+        servos.P0.stop()
+    } else if (receivedNumber == 666) {
+        servos.P1.stop()
+        servos.P0.stop()
+    }
+})
+radio.setGroup(255)
+basic.forever(function () {
+	
+})
